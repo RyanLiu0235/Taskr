54ea0dc18831fbcc75964e3086c83e945e2f206c
stop2stare <emitremmus0235@163.com>
2017-03-22 01:13:29 +0800

    comments

--- app/src/main/store/index.js
+++ app/src/main/store/index.js
@@ -1,5 +1,11 @@
 const fs = require('fs')
 
+/**
+ * 本地存储任务
+ *
+ * @param  {Object}   data
+ * @param  {Function} cb
+ */
 export const storeTasks = function (data, cb) {
   fs.writeFile('./store.json', JSON.stringify(data), (err) => {
     if (err) {
@@ -12,6 +18,11 @@ export const storeTasks = function (data, cb) {
   })
 }
 
+/**
+ * 读取本地存储的任务
+ *
+ * @param  {Function} cb
+ */
 export const getTasks = function (cb) {
   fs.readFile('./store.json', 'utf8', (err, data) => {
     if (err) {
