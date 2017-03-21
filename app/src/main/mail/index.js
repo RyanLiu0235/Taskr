81e9decd5569dad6fb85db3a7b47f9646f98bd98
stop2stare <emitremmus0235@163.com>
2017-03-22 00:44:36 +0800

    generate table for weekly report

--- app/src/main/mail/index.js
+++ app/src/main/mail/index.js
@@ -3,7 +3,14 @@ import { server, mailOptions } from './config.mail'
 
 const transporter = nodemailer.createTransport(server)
 
+/**
+ * 整理、发送邮件
+ *
+ * @param  {Array}   data
+ * @param  {Function} cb
+ */
 export default function (data, cb) {
+  mailOptions['html'] = genTable(data)
   // 发送邮件
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
@@ -16,4 +23,37 @@ export default function (data, cb) {
       info
     })
   })
+}
+
+/**
+ * 依据列表生成表格
+ *
+ * @param  {Array} list
+ * @return {String}
+ */
+function genTable (list) {
+  const thead = '<thead>' +
+    '<tr>' +
+    '<th>项目</th>' +
+    '<th>版本号</th>' +
+    '<th>进度</th>' +
+    '<th>备注</th>' +
+    '</tr>' +
+    '</thead>'
+
+  const tbody = '<tbody>' +
+    list.map((item, index) =>
+      '<tr>' +
+      `<td>${item.name}</td>` +
+      `<td>${item.version}</td>` +
+      `<td>${item.progress}</td>` +
+      `<td>${item.remarks}</td>` +
+      '</tr>'
+    ) +
+    '</tbody>'
+
+  return '<table>' +
+    thead +
+    tbody +
+    '</table>'
 }
